749f4e2d9324ac841aead242bdc9acb0b4209248
eezzo <derastafari@gmail.com>
2020-09-08 21:47:28 -0500

    class backbone finished minus notific, taskqueue

--- anime_site_util.py
+++ anime_site_util.py
@@ -1,44 +1,78 @@
 class StreamingSite:
+    def __init__(self, url, ep_id_template):
+        self.url = url
+        self.ep_id_template = ep_id_template
+
     def get_episode_streaming_link(self, anime):
         pass
+
 class AnimeListSite:
-    def can_connect(self):
-        pass
-    def auth_user(self):
-        pass
-    def get_user_anime(self, user):
-        import requests
-        # iss: needs to get content (currently viewing) from all lists and merge them
-        query = '''
-            query ($id: Int) {
-                MediaListCollection(userId: $id, type:ANIME, status:CURRENT){
-                    lists {
-                        entries {
-                            media {
-                                id
-                                title {
-                                    userPreferred
-                                }
-                                coverImage {
-                                    medium
-                                }
-                                nextAiringEpisode{
-                                    episode
+    import conf
+
+    def __init__(self, endpoint_url='https://graphql.anilist.co',
+                 # auth stuff is for site's anime data retrieval requirements, not end user auth stuff
+                 req_auth=False, auth_req_captcha=False, auth_user=conf.uname, auth_pass=conf.pw, token=None,
+                 query_method='POST',
+                 anime_query_template='',
+                 anime_bulk_query_template='',
+                 user_anime_list_query_template='''
+                            query ($id: Int) {
+                                MediaListCollection(userId: $id, type:ANIME, status:CURRENT){
+                                    lists {
+                                        entries {
+                                            media {
+                                                id
+                                                title {
+                                                    userPreferred
+                                                }
+                                                coverImage {
+                                                    medium
+                                                }
+                                                nextAiringEpisode{
+                                                    episode
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
                             }
-                        }
-                    }
-                }
-            }
-            '''
+                            '''
+                 ):
+
+        self.endpoint_url = endpoint_url
+        self.req_auth = req_auth
+        self.auth_req_captcha = auth_req_captcha
+        self.auth_user = auth_user
+        self.auth_pass = auth_pass
+        self.token = token
+
+        self.query_method = query_method
+        self.anime_query_template = anime_query_template
+        self.anime_bulk_query_template = anime_bulk_query_template
+        self.user_anime_list_query_template = user_anime_list_query_template
+
+    def can_connect(self):
+        pass
+
+    def auth_user(self):
+        pass
+
+    def get_anime_data(self, anime_id):
+        pass
+
+    def get_bulk_anime_data(self, anime_id):
+        pass
+
+    def get_user_anime_list(self, user):
+        import requests
 
         variables = {
             'id': user
         }
 
-        url = 'https://graphql.anilist.co'
+        url = self.endpoint_url
 
-        response = requests.post(url, json={'query': query, 'variables': variables})
+        response = requests.post(url, json={'query': self.user_anime_list_query_template, 'variables': variables})
 
         json_response = response.json()
 
@@ -46,92 +80,113 @@ class AnimeListSite:
 
         return user_anime_data
 
-class AnimeChecker:
-    #def start(self):
-        #while True:
-            #get_all_subscribed_anime
-            #for each
-                # check if airing
-                    # get num released ep
-                    # check_new_episode
-                    # if new
-                        # send notification and update num_released episodes
-    def stop(self):
-        pass
-    def get_status(self):
-        pass
-    def add_anime(self, id):
-        ''' adds anime from list to check for new eps '''
-        pass
-        # will add this later when I've learned more about abstracting data interfaces
-    def remove_anime(self, id):
-        ''' removes anime from list to check for new eps '''
-        pass
-        # will add this later when I've learned more about abstracting data interfaces
-    def get_all_subscribed_anime(self):
-        ''' gets anilist IDs of all anime that users have requested to be notified about '''
-        # will add this later when I've learned more about abstracting data interfaces
-        # using static list of current seasons anime for now
-        subscribed_anime = [21,97940,108632,116006,97938]
-        return subscribed_anime
+class Anime:
 
-    def is_airing(self, anime_dict):
-        ''' returns true if currently airing, else false '''
-        if anime_dict['status'] == 'RELEASING':
+    # add status tuples here (for our standard data format)
+    def __init__(self, anilist_id, title, status, tot_num_ep,# iss: pick which params are req and default vals
+                 next_air_ep, time_until_air, ext_links,
+                 cover_med_imglink, cover_lg_imglink, cover_xl_imglink, last_updated,
+                 subscribers):
+        self.anilist_id = anilist_id
+        self.title = title
+        self.status = status
+        self.tot_num_ep = tot_num_ep
+        self.next_air_ep = next_air_ep
+        self.time_until_air = time_until_air
+        self.ext_links = ext_links
+        self.cover_med_imglink = cover_med_imglink
+        self.cover_lg_imglink = cover_lg_imglink
+        self.cover_xl_imglink = cover_xl_imglink
+        self.last_updated = last_updated
+
+        #this may be bad practice, check
+        self.num_episodes_released = self.get_num_eps_released()
+        self.latest_ep_stream_link = self.get_latest_ep_stream_link()
+
+        # Relationships with other objs
+        self.subscribers = subscribers # these should be user objects
+        # question: should there be relationship between anime and streamingsite?
+
+    #this method might call AnimeSite method or StreamingSite method
+    def get_latest_ep_stream_link(self, stream_site):
+        pass
+
+    #this method might call AnimeSite method or StreamingSite method
+    def get_released_ep_stream_link(self, stream_site):
+        pass
+
+    def get_anime_status(self):
+        """ gets anime status when last checked """
+        status = self.status
+        return status
+
+    def is_airing(self):
+        """ returns true if airing, else false """
+        status = self.status
+        if status == 'RELEASING':
             return True
         return False
 
-    def get_anime_status(self, anime_dict):
-        ''' returns true if currently airing, else false '''
-        status = anime_dict['status']
-        return status
-
-    def check_new_episode(self, prev_num_episodes, anime_dict):
-        ''' returns true if new episode has been released '''
-        pass
-        # if no prev_num_episodes
-
-    def get_num_released_episodes(self, anime_dict):
-        ''' returns the number of released episodes for anime '''
-        status = self.get_anime_status(anime_dict)
+    def get_num_eps_released(self):
+        """ returns the number of released episodes for anime """
+        status = self.status
         if status == 'RELEASING':
-            next_airing = anime_dict["nextAiringEpisode"]["episode"]
-            latest_episode = next_airing - 1
+            next_airing = self.next_air_ep
+            latest_episode_num = next_airing - 1
         elif status == 'NOT_YET_RELEASED':
-            latest_episode = 0
+            latest_episode_num = 0
         elif status == 'CANCELLED' or status == 'FINISHED':
-            latest_episode = anime_dict["episodes"]
-            if not latest_episode:
-                latest_episode = 0
-        return latest_episode
+            latest_episode_num = self.tot_num_ep
+            if not latest_episode_num:
+                latest_episode_num = 0
+        return latest_episode_num
 
-    def get_anime_streaming_sites(self):
+    def check_new_episode(self, anime_site):
+        ''' returns true if new episode has been released
+        may need to update self.info before returning
+        '''
         pass
 
-class Anime:
-    def __init__(self, id, num_episodes):
-        self.id = id
-        self.num_episodes_released = num_episodes
+    def get_stream_opts(self, anime_site):
+        """
+        checks external links for known streaming sites and returns them in a list of dicts
+            {
+                "name":"CrunchyRoll",
+                "url":"https://crunchyroll.com/<anime-name>",
+            },
+        """
+        pass
 
-    def update_data(self):
+    def update_data(self, anime_site):
         pass
-        # not sure if needed
-    def is_airing(self):
-        pass
-        # not sure if needed
 
 class User:
 
-    def __init__(self, email, phone, site_tokens):
+    def __init__(self, email, phone, site_tokens_dict=None, anime_list=None):
         self.email = email
         self.phone = phone
-        self.site_tokens = site_tokens
 
-    def get_user_anime(self, site):
+        # relationships with obj
+        self.site_tokens_dict = site_tokens_dict # site tokens, not an obj
+        self.anime_list = anime_list
+
+    def get_saved_user_anime_list(self):
+        """ gets local user anime list if it exists (must have called get_fresh_user_anime_list) """
+        anime_list = self.anime_list
+        return anime_list
+
+    def get_fresh_user_anime_list(self):
         ## return, id, title, image, num_episodes released, time til next release
+        # for each site_token
+            #create site obj and call isauth
+            # if isauth call site.getuseranime()
+            # win win win no matta what
+            # create anime obj and add to self.anime_list
         pass
-    def has_auth_token(self):
+
+    def has_site_token(self):
         pass
+
     def get_synced_sites(self):
         pass
 
---
+++ conf.py
@@ -0,0 +1,4 @@
+# configuration file for anime site connection stuff
+
+UNAME = 'ENTER_USERNAME_HERE'
+PW = 'ENTER_PW_HERE'
